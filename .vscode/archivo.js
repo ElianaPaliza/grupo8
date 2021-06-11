69cd7f7e9843e727ca6a482cdc68a38df17e3075
Eliana Paliza <eloshady.459@gmail.com>
2021-06-11 08:29:44 -0300

    funcionaje

--- .vscode/archivo.js
+++ .vscode/archivo.js
@@ -10,3 +10,7 @@ function dividir(a,b){
     return a / b
 };
 console.log(dividir(10,2))
+dsfgksdg
+gd
+sdag
+das
--- .vscode/hola.js
+++ .vscode/hola.js
@@ -1,3 +1,7 @@
 function saludar() {
     return 'Hola';
 }
+function sumar(a,b){
+    return a + b
+}
+console.log(sumar(10,5))
